edd229627b2415d5bd804fcccc2d6a2d18e5d68a
Bastien Falcou <bastien.falcou@compass.com>
2019-11-09 19:18:05 -0500

    refactor(AppDelegate): simplify code and handle injection

--- Portfolio/AppDelegate.swift
+++ Portfolio/AppDelegate.swift
@@ -10,9 +10,17 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    private lazy var serviceFactory: ServiceFactory = {
+        return ServiceFactory()
+    }()
+    private lazy var apiServiceFactory: APIServiceFactory = {
+        return APIServiceFactory()
+    }()
+    private lazy var viewControllerFactory: ViewControllerFactory = {
+        return ViewControllerFactory(serviceFactory: serviceFactory, apiServiceFactory: apiServiceFactory)
+    }()
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-
         return true
     }
 }
