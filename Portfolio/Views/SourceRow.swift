f24329c826f99f19d18220d0d64104c37d7fea76
Bastien Falcou <bastien.falcou@compass.com>
2019-11-16 20:04:00 -0500

    feat(SourceView): introduce even more varied values for preview

--- Portfolio/Views/SourceRow.swift
+++ Portfolio/Views/SourceRow.swift
@@ -38,15 +38,14 @@ struct SourceRow_Previews: PreviewProvider {
                                currency: 0,
                                amount: 8000)
                 )
-            )
+            ).previewLayout(.fixed(width: 200, height: 120))
             SourceRow(viewModel: SourceRowViewModel(
                 source: Source(name: "Source With A Very Long name So Long It goes Out",
                                source: 1,
                                currency: 0,
                                amount: 100000)
                 )
-            )
+            ).previewLayout(.fixed(width: 350, height: 70))
         }
-        .previewLayout(.fixed(width: 300, height: 70))
     }
 }
